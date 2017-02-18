1b586bfd5ff67bb6c061a7fbfbfca3e985df5d25
LuckyKoala <1224393412@qq.com>
2017-02-18 13:01:09 +0800

    add cli.result method to show message about the result of command operation

--- cli.js
+++ cli.js
@@ -5,8 +5,22 @@ mod.response = function(message) {
     let prefix = dye(CRAYON.cli, 'CLI > ');
     console.log( prefix + message );
 };
+mod.result = (returnVal)=>returnVal?mod.success():mod.fail();
+mod.success = ()=>mod.response('Command Operate Success.');
+mod.fail = ()=>mod.response('Command Operate Fail.');
+
 mod.help = function() {
 	mod.response("Help Menu Response");
 	mod.response("creep(name),shorthand of Game.creeps[name]");
+	mod.response("spawn(setup),shorthand of Game.spawns['<spawnName>'].createCreepBySetup(Creep.setup.worker)");
 }
-mod.creep = name=>Game.creeps[name];
+mod.creep = function(name) {
+	let creep = Game.creeps[name];
+	mod.result(creep);
+	return creep;
+};
+mod.spawn = function(spawnName,setup) {
+	let returnVal = Game.spawns[spawnName];
+	mod.result(returnVal);
+	return returnVal;
+};
