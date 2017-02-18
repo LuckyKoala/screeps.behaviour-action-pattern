dea2a58201f0b8a6e71d710939a74f0fde6172da
LuckyKoala <1224393412@qq.com>
2017-02-18 12:28:55 +0800

    add response method and creep shorthand

--- cli.js
+++ cli.js
@@ -1,6 +1,12 @@
 let mod = {};
 module.exports = mod;
 
+mod.response = function(roomName, message) {
+    let text = dye(CRAYON.system, roomName);
+    console.log( dye(CRAYON.system, message );
+};
 mod.help = function() {
-	console.log('Help Menu Response');
+	response("Help Menu Response");
+	response("creep(name),shorthand of Game.creeps[name]");
 }
+mod.creep = name=>Game.creeps[name];
